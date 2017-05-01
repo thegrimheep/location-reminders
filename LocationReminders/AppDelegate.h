bf86546930ac8e338bffb69c88599def575831d3
David <davidporter33@icloud.com>
2017-05-01 10:47:13 -0700

    Initial Commit

---
+++ LocationReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminders
+//
+//  Created by David Porter on 5/1/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationReminders
+//
+//  Created by David Porter on 5/1/17.
+//  Copyright © 2017 David Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
