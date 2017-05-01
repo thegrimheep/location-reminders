3932c38c2d153bd3f1ab7c4406249a488383e926
David <davidporter33@icloud.com>
2017-05-01 13:55:20 -0700

    Added a button that takes you to the coordinates for my home.

--- LocationReminders/ViewController.m
+++ LocationReminders/ViewController.m
@@ -13,6 +13,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -21,18 +22,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    
-    testObject[@"testName"] = @"David Porter";
-    
-    [testObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
-        if (succeeded) {
-            NSLog(@"Success Saving test object");
-        } else {
-            NSLog(@"There was a problem saving. Save Error: %@", error.localizedDescription);
-        }
-    }];
+//    
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//    
+//    testObject[@"testName"] = @"David Porter";
+//    
+//    [testObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"Success Saving test object");
+//        } else {
+//            NSLog(@"There was a problem saving. Save Error: %@", error.localizedDescription);
+//        }
+//    }];
     
 //    PFQuery *query = [PFQuery queryWithClassName:@"TestObject"];
 //    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
@@ -42,8 +43,22 @@
 //            NSLog(@"Query Results %@", objects);
 //        }
 //    }];
+    
+    [self requestPermissions];
+    self.mapView.showsUserLocation = YES;
 }
+
+-(void)requestPermissions {
+    self.locationManager = [[CLLocationManager alloc]init];
+    [self.locationManager requestAlwaysAuthorization];
+}
+
 - (IBAction)location1Pressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.5600088, -122.3906479);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+    
+    [self.mapView setRegion:region animated:YES];
 }
 
 
